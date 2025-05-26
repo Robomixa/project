92830d33b11d85020b1851e30399d11c061e97b8
Robomixa <veronika.po2006@gmail.com>
2025-05-27 02:04:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -253,4 +253,6 @@ def update_status(app_id, new_status):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    debug_mode = os.getenv('FLASK_DEBUG', 'False').lower() in ['true', '1', 'yes']
+    app.run(debug=debug_mode)
